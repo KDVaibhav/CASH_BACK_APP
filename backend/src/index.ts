0445a31a547e9a67f878edfa670f77e0759db2dd
Vaibhav Agrawal <va21032002@gmail.com>
2025-11-07 20:47:00 +0530

    Project Bootstrapped

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import apiRouter from "./routes";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+
+const MONGO_URI = process.env.MONGO_URI!;
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("Server connected to DB");
+  } catch (err) {
+    console.error("Error connecting to MongoDb", err);
+  }
+};
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", apiRouter);
+
+connectDb().then(() => {
+  app.listen(4000, () => console.log("Server Running on port 4000"));
+});
